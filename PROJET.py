02abc3a22bcacc44458fcbb35d948c1b7253179b
Fatima AIT-IGHIL <fatima.ait-ighil@ens.uvsq.fr>
2021-04-15 13:53:53 +0200

    essaie

---
+++ PROJET.py
@@ -0,0 +1,19 @@
+#############################################
+# groupe BI1
+# Laurie MIALIN
+# Fatima AIT IGHIL 
+# Cecile FETRE
+# Charoline JOSEPH
+# https://github.com/uvsq22007110/projet2_generation_d-un_terrain_de_jeu_videoexis BRIAND
+#############################################
+from tkinter import *
+#############################################
+#definition des constantes(M)
+
+#############################################
+#definition des variables globales
+#############################################
+#definition des fonctions
+#############################################
+#programme principal
+#############################################
