4cc3b0801143defcf0724a4632ec28ec1567d33b
Fatima AIT-IGHIL <fatima.ait-ighil@ens.uvsq.fr>
2021-04-15 14:35:32 +0200

    essaie

--- PROJET.py
+++ PROJET.py
@@ -4,11 +4,19 @@
 # Fatima AIT IGHIL 
 # Cecile FETRE
 # Charoline JOSEPH
-# https://github.com/uvsq22007110/projet2_generation_d-un_terrain_de_jeu_videoexis BRIAND
+# Aexis BRIAND
+# Mecipssa HESNI
+# https://github.com/uvsq22007110/projet2_generation_d-un_terrain_de_jeu_video
 #############################################
-from tkinter import *
+import tkinter as tk
 #############################################
 #definition des constantes(M)
+p = 0.5
+n = 4
+T = 5
+k = 1
+HEIGHT = 500
+WIDTH = 500
 
 #############################################
 #definition des variables globales
@@ -16,4 +24,8 @@ from tkinter import *
 #definition des fonctions
 #############################################
 #programme principal
+racine = tk.Tk()
+canvas = tk.Canvas(racine, bg = "black", width = WIDTH, height = HEIGHT)
+canvas.grid()
+racine.mainloop()
 #############################################
