ce435477b950474a268e329346ae54f989d009fb
Cécile FETRE <cecile.fetre@ens.uvsq.fr>
2021-04-29 18:12:49 +0200

    choix_taille + Button Taille

--- PROJET.py
+++ PROJET.py
@@ -18,6 +18,7 @@ T = 5
 k = 1
 HEIGHT = 800
 WIDTH = 800
+nb_cases = 50
 cpt = 0
 
 #############################################
@@ -29,12 +30,12 @@ cpt = 0
 def grille(cases):
     """génération de la grille"""
     CG=coulour_gille(cases)
-    for i in range(50):
-        for j in range(50):
-            pos1x = i * (WIDTH/50)            
-            pos1y = j * (HEIGHT/50)
-            pos2x = (i + 1)*(WIDTH/50)
-            pos2y = (j + 1)*(HEIGHT/50)
+    for i in range(nb_cases):
+        for j in range(nb_cases):
+            pos1x = i * (WIDTH/nb_cases)            
+            pos1y = j * (HEIGHT/nb_cases)
+            pos2x = (i + 1)*(WIDTH/nb_cases)
+            pos2y = (j + 1)*(HEIGHT/nb_cases)
             colors=rd.choice(CG)
             CG.remove(colors) 
             case = grillage.create_rectangle((pos1x, pos1y), (pos2x, pos2y), fill = colors)
@@ -42,8 +43,8 @@ def grille(cases):
 
 def coulour_gille (p) :
     """ crée une list de 2500 elements remplit de blue et brown pour générer notre terrain """
-    nombre_eau=2500*p
-    nombre_terre=2500-nombre_eau
+    nombre_eau=nb_cases*nb_cases*p
+    nombre_terre=(nb_cases*nb_cases)-nombre_eau
     Lbleu=["blue"]*int(nombre_eau)
     Lbrown=["brown"]*int(nombre_terre)
     Lcolors=Lbleu+Lbrown
@@ -83,7 +84,9 @@ def choix_parametres():
 
 def choix_taille():
     """l'utilisateur choisit la taille de la grille"""
-    pass
+    global nb_cases
+    nb_cases = int(input("Choisir le nombre de cases de la grille"))
+    grille(p)
 
 def annuler_deplacement():
     """annule le déplacement du personnage"""
@@ -102,13 +105,15 @@ def recharge():
 #############################################
 #programme principal
 racine = tk.Tk()
+Taille = tk.Button(racine, text="Taille de la grille", bd=10, bg="grey", fg="black", activeforeground="white", activebackground="red", command=choix_taille, padx=10, pady=10, relief="groove")
+Taille.grid(row=0, column=1)
 grillage = tk.Canvas(racine, bg = "white", width = WIDTH, height = HEIGHT)
-grillage.grid()
+grillage.grid(row=0, column=0)
 grillage.bind("<Button-1>", cercle)
-grillage.bind("<Up>", haut)
-grillage.bind("<Down>", bas)
-grillage.bind("<Left>", gauche)
-grillage.bind("<Right>", droite)
+# grillage.bind("<Up>", haut)
+# grillage.bind("<Down>", bas)
+# grillage.bind("<Left>", gauche)
+# grillage.bind("<Right>", droite)
 grille(p)
 b=grillage.find_all()
 racine.mainloop()
